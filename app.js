5e794e97f560fea18269d4eaa944484ab1dc5acb
Harry Iliffe <iliffe.harry@gmail.com>
2019-06-15 17:49:09 +1200

    added database viewer and ability to add objects

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
 // app.js
 //EXPRESS STUFF
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-const bodyParser = require('body-parser');
+const formidableMiddleware = require('express-formidable');
+
+//HELPER STUFF
+const underscore = require('underscore');
+const shortid = require('shortid');
+
 //DATABASE STUFF
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('db/db.json')
-const db = low(adapter)
-db._.mixin(require('underscore-db'))
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db/db.json');
+
+const db = low(adapter);
+
+db._.mixin(require('lodash-id'));
 
 
-// db.defaults({ items: [], user: {}, count: 0 })
-//   .write()
+db._.mixin({
+  createId: function(collectionName, doc) {
+    return shortid.generate();
+  }
+});
 
-// db.set('user.name', 'harry')
-//     .write()
+db.defaults({ items: [], user: {}, count: 0 })
+  .write()
 
-// db.get('items')
-//   .push({ id: 2, title: 'Xbox Controller', description: 'Xbox 360 controller', image:"xboxcontroller.jpg"})
-//   .write()
-//
-// db.update('count', n => n + 1)
-//   .write()
+db.set('user.name', 'harry')
+    .write()
+
 
 
 //SETUP EXPRESS + HANDLEBARS
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(formidableMiddleware({
+    uploadDir: 'tmp_uploads',
+    multiples: true, // req.files to be arrays of files
+    keepExtensions: true
+}));
+
+app.use(express.static('public'));
 
 
 //ROUTE THINGS
 app.get('/', function (req, res) {
-    var items = db.get('items').value(); // Find all users in the collection
+    var items = db.get('items').value(); // Find all items in the collection
     var user = db.get('user').value()
     res.render('home', {
       title: "Whole House Project",
-      style: "bootstrap.css",
       user: user,
       items: items
 
@@ -52,27 +65,72 @@ app.get('/', function (req, res) {
 app.get('/add', function (req, res) {
     res.render('add', {
       title: "Whole House Project",
-      style: "bootstrap.css",
       name: "Harry"
 
     });
 });
 
+app.get("/view", function(req, res) {
+  item = db.get('items')
+           .getById(req.query.itemID)
+           .value();
+  console.log(item);
+
+  res.render('view', {
+    title: "Whole House Project",
+    name: "Harry",
+    item: item
+  });
+});
+
 app.post('/submit', function (req, res) {
-  const data = req.body;
-  console.log(data);
+  console.log("Received Data")
 
-  db.get('items')
-    // .push({ id: 2, title: 'Xbox Controller', description: 'Xbox 360 controller', image:"xboxcontroller.jpg"})
+  const data = req.fields
+
+  //add data to db
+  itemID = db.get('items')
     .insert(data)
+    .write().id
 
-    .write()
+  //proccess files
+  const filename = db.get('items')
+                     .getById(itemID)
+                     .value().title
 
-  res.end()
+  const files = req.files.photos
+
+  //make directory
+  folder = path.join( 'public/images/' +itemID)
+  if(!fs.existsSync(folder)){
+    fs.mkdir(folder);
+  }
+
+
+  //move files
+  if(Array.isArray(files) && files.length>0){
+    console.log("Multiple files detected")
+  } else {
+    n = "";
+    while(fs.existsSync(path.join(folder, filename + n+".jpg"))){
+      n => n + 1;
+    }
+    fs.rename(files.path, path.join(folder, filename + n+".jpg"));
+
+    db.get('items')
+      .getById(itemID)
+      .assign({imagePath:[]})
+      .get("imagePath")
+      .push(path.join('images/',itemID, filename + n+".jpg"))
+      .write()
+  }
+  console.log("Data Received. Added Item: " + itemID);
+  res.send(itemID)
+
 });
 
 
 
 const server = app.listen(3000, ()=> {
-  console.log('express-handlebars example server listening on: 3000');
+  console.log('Whole House Project listening on: 3000');
 });
