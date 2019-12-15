da7aac19496dea611ebe3674f85243c9fd071bdb
Harry Iliffe <iliffe.harry@gmail.com>
2019-12-15 20:44:08 +1300

    added missing db folder to install script

--- scripts/makedirs.js
+++ scripts/makedirs.js
@@ -9,7 +9,13 @@ mkdirp('./src/public/assets', function (err) {
     if (err) console.error(err)
     else console.log('Created assets folder')
 });
+
 mkdirp('./src/tmp_uploads', function (err) {
     if (err) console.error(err)
     else console.log('Created tmp_uploads folder')
+});
+
+mkdirp('./src/db', function (err) {
+    if (err) console.error(err)
+    else console.log('Created db folder')
 });
